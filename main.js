a5ffdcd6af404d4d7972dc8857e7734307037c99
khantuevao <khantuevao@gmail.com>
2022-09-29 12:41:40 +0200

    Configure header&footer styling for wide screens

---
+++ main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('it works')
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLHVCIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vbWFnaWMtbWFyYmxlLy4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKCdpdCB3b3JrcycpIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
